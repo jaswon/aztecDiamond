dc0a3e019ced19191384c794a87b4accd4afe08b
jaswon5791 <jasonwon628@gmail.com>
2020-12-25 19:03:13 -0500

    get canvas

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("canvas")!;
+    canvas.width = 1080;
+    canvas.height = 1080;
+
+    const ctx = canvas.getContext("2d")!;
+
+})
